e6df7b3ee76f0868d3039e4e8d993cb9fae3d527
Sergiu <Sergiu@LAPTOP-UH2BJPH9>
2020-04-05 14:29:40 +0300

    Add project files.

---
+++ Size of File_Folder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Size_of_File_Folder
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+          string[] a=  Directory.GetFiles(@"C:\POB\Size of File_Folder\Size of File_Folder","TextFile1.txt");
+            string FileName = a[0];
+            FileInfo fi = new FileInfo(FileName);
+            long sizeb = fi.Length;
+            
+            Console.WriteLine("Dimensiunea in bytes:{0}", sizeb);
+            Console.WriteLine("Dimensiunea pe disk:{0}", a);
+
+        }
+    }
+}
